4a4c8d733181fa37a1658913bf375df0b7989551
Juraj Mičko <micko.juraj64@gmail.com>
2015-12-17 19:51:21 +0100

    fixed moveCursor method of CharLCD

--- src/main/java/com/jjurm/libs/charlcd/CharLCD.java
+++ src/main/java/com/jjurm/libs/charlcd/CharLCD.java
@@ -367,7 +367,7 @@ public class CharLCD {
 			throw new IllegalArgumentException(String.format("Requested cursor position (%d, %d) is out of range", col, row));
 		}
 		// row offsets are [0x00, 0x40, 0x00 + cols, 0x40 + cols]
-		int addr = ((row & 0x01) * 0x40 + (row & 0x02) * cols) + cols;
+		int addr = ((row & 0x01) * 0x40 + ((row >> 1) & 0x01) * cols) + col;
 		command(CMD.SETDDRAMADDR.bits
 				| addr
 		);
